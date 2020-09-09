3775d5d21f7977280ffbded8450d69a9ab811366
Onigiri <steinsgate.yn@gmail.com>
2020-09-09 15:37:51 +0800

    删除链表中的节点

---
+++ 0237_delete-node-in-a-linked-list.py
@@ -0,0 +1,47 @@
+"""
+删除链表中的节点
+
+链接：https://leetcode-cn.com/problems/delete-node-in-a-linked-list
+
+请编写一个函数，使其可以删除某个链表中给定的（非末尾）节点。传入函数的唯一参数为要被删除的节点。
+
+现有一个链表 --head = [4,5,1,9]，它可以表示为: 4 -> 5 -> 1 -> 9
+
+示例 1：
+输入：head = [4,5,1,9], node = 5
+输出：[4,1,9]
+解释：给定你链表中值为5的第二个节点，那么在调用了你的函数之后，该链表应变为 4 -> 1 -> 9.
+
+示例 2：
+输入：head = [4,5,1,9], node = 1
+输出：[4,5,9]
+解释：给定你链表中值为1的第三个节点，那么在调用了你的函数之后，该链表应变为 4 -> 5 -> 9.
+
+提示：
+链表至少包含两个节点。
+链表中所有节点的值都是唯一的。
+给定的节点为非末尾节点并且一定是链表中的一个有效节点。
+不要从你的函数中返回任何结果。
+
+我的解题思路：
+1. 将目标节点的 val 和 next 都变成下一个。
+
+"""
+import unittest
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def delete_node(self, node: ListNode) -> None:
+        node.val = node.next.val
+        node.next = node.next.next
+
+
+if __name__ == '__main__':
+    unittest.main()
