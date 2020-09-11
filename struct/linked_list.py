f41476f90d740fd26c219ae90b7b4a6bb95c8cc5
Onigiri <steinsgate.yn@gmail.com>
2020-09-11 12:43:23 +0800

    feat: 新增单链表数据结构

---
+++ struct/linked_list.py
@@ -0,0 +1,17 @@
+from typing import List
+
+
+# 单链表的定义.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+def list_to_list_node(arr: List) -> ListNode:
+    dummy = ListNode(0)
+    n = dummy
+    for v in arr:
+        n.next = ListNode(v)
+        n = n.next
+    return dummy.next
