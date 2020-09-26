06bf7c67c0f8a54a79cc58229665be61ae633321
Onigiri <steinsgate.yn@gmail.com>
2020-09-26 11:49:37 +0800

    两数相加：按位相加

--- 0002_add_two_numbers.py
+++ 0002_add_two_numbers.py
@@ -35,11 +35,11 @@ class Solution:
 
         # 标识下一节点是否需要进位。
         plus = False
-        while l1 is not None or l2 is not None:
+        while l1 or l2:
             val = 0
-            if l1 is not None:
+            if l1:
                 val += l1.val
-            if l2 is not None:
+            if l2:
                 val += l2.val
             if plus:
                 val += 1
@@ -52,8 +52,8 @@ class Solution:
             node.next = ListNode(val)
             node = node.next
 
-            l1 = l1.next if l1 is not None else l1
-            l2 = l2.next if l2 is not None else l2
+            l1 = l1.next if l1 else l1
+            l2 = l2.next if l2 else l2
 
         # 末尾进位。
         if plus:
