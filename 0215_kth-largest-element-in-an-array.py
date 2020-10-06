dbe18de2512be65a0739f7e6d7ffaf0defec35aa
Onigiri <steinsgate.yn@gmail.com>
2020-10-06 19:10:42 +0800

    数组中的第 k 个最大元素：基于快速排序的选择方法

--- 0215_kth-largest-element-in-an-array.py
+++ 0215_kth-largest-element-in-an-array.py
@@ -23,6 +23,12 @@
 
 2. 先排序。
 
+官方解法：
+1. 基于快速排序的选择方法。
+对原数组快排的分区过程中，只要某次用于划分左右区域的下标 q 为倒数第 k 个下标时，就得到了答案。
+
+时间复杂度：O(N)，如上文所述，证明过程可以参考「《算法导论》9.2：期望为线性的选择算法」。
+空间复杂度：O(logN)，递归使用栈空间的空间代价的期望为 O(logN)。
 
 """
 import unittest
@@ -39,6 +45,37 @@ class Solution:
             if len(heap) > k:
                 heapq.heappop(heap)
         return heapq.heappop(heap)
+
+
+class OfficialSolution:
+    def find_kth_largest(self, nums: List[int], k: int) -> int:
+        """基于快速排序的选择方法。"""
+        n = len(nums)
+        target_index = n - k
+        left, right = 0, n - 1
+        while True:
+            index = self.partition(nums, left, right)
+            if index == target_index:
+                return nums[index]
+            elif index < target_index:
+                # 下一轮在 [index + 1, right] 中找。
+                left = index + 1
+            else:
+                right = index - 1
+
+    def partition(self, nums: List[int], left, right: int) -> int:
+        """快排分区。"""
+        pivot = left
+        i, j = left, right
+        while i < j:
+            while i < j and nums[j] > nums[pivot]:
+                j -= 1
+            while i < j and nums[i] <= nums[pivot]:
+                i += 1
+            nums[i], nums[j] = nums[j], nums[i]
+        # 此时 i 等于 j。
+        nums[j], nums[pivot] = nums[pivot], nums[j]
+        return j
 
 
 class TestSolution(unittest.TestCase):
