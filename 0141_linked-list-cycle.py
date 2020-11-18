4b1c993c925e97265949f5832ff29537486fbf5a
Onigiri <steinsgate.yn@gmail.com>
2020-11-18 10:00:40 +0800

    环形链表：快慢指针（二刷）

--- 0141_linked-list-cycle.py
+++ 0141_linked-list-cycle.py
@@ -37,6 +37,7 @@
 
 官方解题方案：
 1. 快慢指针
+
 时间复杂度：O(n)，让我们将 n 设为链表中结点的总数。为了分析时间复杂度，我们分别考虑下面两种情况。
  - 链表中不存在环：
    快指针将会首先到达尾部，其时间取决于列表的长度，也就是 O(n)。
@@ -50,12 +51,13 @@
       因此需要经过 "二者之间距离/速度差值" 次循环后，快跑者可以追上慢跑者。
       这个距离几乎就是 "环形部分长度 K" 且速度差值为 1，我们得出这样的结论 "迭代次数=近似于 '环形部分长度 K'".
 因此，在最糟糕的情形下，时间复杂度为 O(N+K)，也就是 O(n)。
-空间复杂度：O(1)O(1)，我们只使用了慢指针和快指针两个结点，所以空间复杂度为 O(1)。
+
+空间复杂度：O(1)，我们只使用了慢指针和快指针两个结点，所以空间复杂度为 O(1)。
 
 """
 import unittest
 
-from structure.linked_list import ListNode, list_to_list_node
+from structure.linked_list import ListNode
 
 
 class Solution:
@@ -66,6 +68,31 @@ class Solution:
                 return True
             d[head] = None
             head = head.next
+        return False
+    
+    def has_cycle_2(self, head: ListNode) -> bool:
+        """
+        快慢指针。
+        慢指针每次走 1 步，快指针每次走 2 步。
+        当快慢指针指向同一个结点时，说明存在环。
+        当快指针指向尾节点时，说明不存在环。
+        """
+        if not head:
+            return False
+        
+        slow, fast = head, head
+        while fast:
+            # 慢指针每次移动 1 步。
+            slow = slow.next
+            # 若快指针 next 为空，说明到了尾结点，即不存在环。
+            if not fast.next:
+                return False
+            # 快指针每次移动 2 步。
+            fast = fast.next.next
+            # 当快慢指针重合时，说明存在环。
+            if fast == slow:
+                return True
+        # 快指针移动到了尾部，不存在环。
         return False
 
 
