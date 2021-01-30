f1f16bb016974407790c59a70f76a81b8f45d0ce
Onigiri <steinsgate.yn@gmail.com>
2021-01-30 19:10:04 +0800

    填充每个节点的下一个右侧节点指针 II：层序遍历

---
+++ 0117_populating-next-right-pointers-in-each-node-ii.py
@@ -0,0 +1,83 @@
+"""
+填充每个节点的下一个右侧节点指针 II
+
+链接：https://leetcode-cn.com/problems/populating-next-right-pointers-in-each-node-ii
+
+给定一个二叉树
+
+struct Node {
+  int val;
+  Node *left;
+  Node *right;
+  Node *next;
+}
+
+填充它的每个 next 指针，让这个指针指向其下一个右侧节点。如果找不到下一个右侧节点，则将 next 指针设置为 NULL。
+
+初始状态下，所有 next 指针都被设置为 NULL。
+
+进阶：
+你只能使用常量级额外空间。
+使用递归解题也符合要求，本题中递归程序占用的栈空间不算做额外的空间复杂度。
+
+示例：
+输入：root = [1,2,3,4,5,null,7]
+输出：[1,#,2,3,#,4,5,7,#]
+解释：给定二叉树如图 A 所示，你的函数应该填充它的每个 next 指针，以指向其下一个右侧节点，如图 B 所示。
+
+提示：
+树中的节点数小于 6000
+-100 <= node.val <= 100
+
+官方解法：
+1. 层序遍历。
+
+时间复杂度：O(N)
+空间复杂度：O(N)
+
+
+2. 使用已建立的 next 指针。
+
+时间复杂度：O(N)
+空间复杂度：O(1)
+
+"""
+import unittest
+from collections import deque
+
+
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+
+
+class Solution:
+    def connect(self, root: Node) -> Node:
+        """层序遍历。"""
+        if not root:
+            return root
+        
+        queue = deque([root])
+        while queue:
+            size = len(queue)
+            for i in range(size):
+                node = queue.popleft()
+                if i < size - 1:
+                    node.next = queue[0]
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+        return root
+
+
+class TestSolution(unittest.TestCase):
+    def setUp(self) -> None:
+        self.s = Solution()
+
+
+if __name__ == '__main__':
+    unittest.main()
