d4ad1574870b2f59099f65066201d9c9edca726f
Onigiri <steinsgate.yn@gmail.com>
2021-04-01 14:25:47 +0800

    排序数组：调整函数顺序

--- 0912_sort-an-array_test.go
+++ 0912_sort-an-array_test.go
@@ -74,6 +74,19 @@ import (
 空间复杂度：O(K)
 是否稳定：√
 排序方式：外排序
+
+9. 桶排序。
+时间复杂度：O(N + K)
+空间复杂度：O(N + K)
+是否稳定：√
+排序方式：外排序
+
+10. 基数排序。
+时间复杂度：O(N * K)
+空间复杂度：O(N + K)
+是否稳定：√
+排序方式：外排序
+
 */
 
 // BubbleSort 冒泡排序。
@@ -211,6 +224,80 @@ func merge(left, right []int) []int {
 	return arr
 }
 
+// QuickSort 基础快速排序。
+func QuickSort(nums []int, left, right int) {
+	if left >= right {
+		return
+	}
+
+	pivot := partition(nums, left, right)
+	QuickSort(nums, left, pivot-1)
+	QuickSort(nums, pivot+1, right)
+}
+
+func partition(nums []int, left, right int) int {
+	pivot := left
+
+	for left < right {
+		// 从右向右移动，找到小于等于 nums[pivot] 的。
+		for left < right && nums[right] > nums[pivot] {
+			right--
+		}
+		// 从左向右移动，找到大于 nums[pivot] 的。
+		for left < right && nums[left] <= nums[pivot] {
+			left++
+		}
+		// 此时 right 刚好指向一个小于等于 nums[pivot] 的值，right 刚好指向一个大于 nums[pivot] 的值，交换它们。
+		nums[left], nums[right] = nums[right], nums[left]
+	}
+	// 此时 left 和 right 指向同一位置，与 pivot 交换。
+	nums[pivot], nums[right] = nums[right], nums[pivot]
+	return right
+}
+
+func swap(nums []int, left, right int) {
+	nums[left], nums[right] = nums[right], nums[left]
+}
+
+// RandomizedQuickSort 随机化快速排序。
+func RandomizedQuickSort(nums []int, left, right int) {
+	if left >= right {
+		return
+	}
+
+	pivot := randomizedPartition(nums, left, right)
+	RandomizedQuickSort(nums, left, pivot-1)
+	RandomizedQuickSort(nums, pivot+1, right)
+}
+
+func randomizedPartition(nums []int, left, right int) int {
+	// 区间范围内随机选择一个基准值下标。
+	randomIdx := rand.Intn(right-left) + left
+	// 将基准值先放到区间最右边。
+	swap(nums, right, randomIdx)
+
+	// i 指向小于基准值的元素。
+	i := left - 1
+	// 遍历区间 [left, right)。
+	for j := left; j < right; j++ {
+		// j 遇到大于等于基准值的元素时，直接右移。
+
+		// j 遇到小于基准值的元素时，将其与 i 指向元素后面的元素交换。
+		// 因为 i + 1 指向的是大于等于基准值的元素。
+		if nums[j] < nums[right] {
+			i++
+			swap(nums, i, j)
+		}
+	}
+
+	// i 此时指向最后一个小于基准值的元素，i + 1 指向第一个大于等于基准值的元素。
+	i++
+	// 将一开始放到区间最右边的基准值与 i 交换。
+	// 交换后 i 指向基准值，i 前面的元素都比基准值小，i 后面的元素都大于等于基准值。
+	swap(nums, i, right)
+	return i
+}
+
 // HeapSort 堆排序。
 func HeapSort(nums []int) {
 	/*
@@ -304,80 +391,6 @@ func CountingSort(nums []int) {
 	}
 }
 
-// QuickSort 基础快速排序。
-func QuickSort(nums []int, left, right int) {
-	if left >= right {
-		return
-	}
-
-	pivot := partition(nums, left, right)
-	QuickSort(nums, left, pivot-1)
-	QuickSort(nums, pivot+1, right)
-}
-
-func partition(nums []int, left, right int) int {
-	pivot := left
-
-	for left < right {
-		// 从右向右移动，找到小于等于 nums[pivot] 的。
-		for left < right && nums[right] > nums[pivot] {
-			right--
-		}
-		// 从左向右移动，找到大于 nums[pivot] 的。
-		for left < right && nums[left] <= nums[pivot] {
-			left++
-		}
-		// 此时 right 刚好指向一个小于等于 nums[pivot] 的值，right 刚好指向一个大于 nums[pivot] 的值，交换它们。
-		nums[left], nums[right] = nums[right], nums[left]
-	}
-	// 此时 left 和 right 指向同一位置，与 pivot 交换。
-	nums[pivot], nums[right] = nums[right], nums[pivot]
-	return right
-}
-
-func swap(nums []int, left, right int) {
-	nums[left], nums[right] = nums[right], nums[left]
-}
-
-// RandomizedQuickSort 随机化快速排序。
-func RandomizedQuickSort(nums []int, left, right int) {
-	if left >= right {
-		return
-	}
-
-	pivot := randomizedPartition(nums, left, right)
-	RandomizedQuickSort(nums, left, pivot-1)
-	RandomizedQuickSort(nums, pivot+1, right)
-}
-
-func randomizedPartition(nums []int, left, right int) int {
-	// 区间范围内随机选择一个基准值下标。
-	randomIdx := rand.Intn(right-left) + left
-	// 将基准值先放到区间最右边。
-	swap(nums, right, randomIdx)
-
-	// i 指向小于基准值的元素。
-	i := left - 1
-	// 遍历区间 [left, right)。
-	for j := left; j < right; j++ {
-		// j 遇到大于等于基准值的元素时，直接右移。
-
-		// j 遇到小于基准值的元素时，将其与 i 指向元素后面的元素交换。
-		// 因为 i + 1 指向的是大于等于基准值的元素。
-		if nums[j] < nums[right] {
-			i++
-			swap(nums, i, j)
-		}
-	}
-
-	// i 此时指向最后一个小于基准值的元素，i + 1 指向第一个大于等于基准值的元素。
-	i++
-	// 将一开始放到区间最右边的基准值与 i 交换。
-	// 交换后 i 指向基准值，i 前面的元素都比基准值小，i 后面的元素都大于等于基准值。
-	swap(nums, i, right)
-	return i
-}
-
 var tests = []struct {
 	nums []int
 	want []int
@@ -419,26 +432,6 @@ func TestInsertionSort(t *testing.T) {
 	}
 }
 
-func TestQuickSort(t *testing.T) {
-	for i := range tests {
-		tc := tests[i]
-		QuickSort(tc.nums, 0, len(tc.nums)-1)
-		if !utils.CompareSlice(tc.nums, tc.want) {
-			t.Fatalf("QuickSort(%+v) return %+v != %+v", tc.nums, tc.nums, tc.want)
-		}
-	}
-}
-
-func TestRandomizedQuickSort(t *testing.T) {
-	for i := range tests {
-		tc := tests[i]
-		RandomizedQuickSort(tc.nums, 0, len(tc.nums)-1)
-		if !utils.CompareSlice(tc.nums, tc.want) {
-			t.Fatalf("RandomizedQuickSort(%+v) return %+v != %+v", tc.nums, tc.nums, tc.want)
-		}
-	}
-}
-
 func TestShellSort(t *testing.T) {
 	for i := range tests {
 		tc := tests[i]
@@ -455,6 +448,26 @@ func TestMergeSort(t *testing.T) {
 		output := MergeSort(tc.nums)
 		if !utils.CompareSlice(output, tc.want) {
 			t.Fatalf("MergeSort(%+v) return %+v != %+v", tc.nums, output, tc.want)
+		}
+	}
+}
+
+func TestQuickSort(t *testing.T) {
+	for i := range tests {
+		tc := tests[i]
+		QuickSort(tc.nums, 0, len(tc.nums)-1)
+		if !utils.CompareSlice(tc.nums, tc.want) {
+			t.Fatalf("QuickSort(%+v) return %+v != %+v", tc.nums, tc.nums, tc.want)
+		}
+	}
+}
+
+func TestRandomizedQuickSort(t *testing.T) {
+	for i := range tests {
+		tc := tests[i]
+		RandomizedQuickSort(tc.nums, 0, len(tc.nums)-1)
+		if !utils.CompareSlice(tc.nums, tc.want) {
+			t.Fatalf("RandomizedQuickSort(%+v) return %+v != %+v", tc.nums, tc.nums, tc.want)
 		}
 	}
 }
